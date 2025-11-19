e6695e49132d3559e5aa74b6713e92e02620f8a2
MariaGrado <al192179@alumnos.uacj.mx>
2025-11-19 16:18:55 -0700

    Refactor tb.v testbench structure and signals
    Refactor testbench for clarity and organization.

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,48 @@
+`timescale 1ns/1ps
 `default_nettype none
-`timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+module tb;
+    reg clk;
+    reg rst_n;
+    reg ena;
+    reg [7:0] ui_in;
+    reg [7:0] uio_in;
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+    // Instancia del diseño
+    tt_um_multiplicador_4x4 dut (
+        .ui_in   (ui_in),
+        .uo_out  (uo_out),
+        .uio_in  (uio_in),
+        .uio_out (uio_out),
+        .uio_oe  (uio_oe),
+        .ena     (ena),
+        .clk     (clk),
+        .rst_n   (rst_n)
+    );
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+    initial begin
+        $dumpfile("tb.fst");
+        $dumpvars(0, tb);
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+        clk = 0;
+        rst_n = 0;
+        ena = 1;
+        uio_in = 8'b0;
+        #20 rst_n = 1;
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+        // Pruebas rápidas
+        ui_in = 8'h00; #40; assert(uo_out == 8'd0);
+        ui_in = 8'h11; #40; assert(uo_out == 8'd1);   // 1×1
+        ui_in = 8'h28; #40; assert(uo_out == 8'd16);  // 2×8
+        ui_in = 8'hF3; #40; assert(uo_out == 8'd45);  // 15×3
+        ui_in = 8'hFF; #40; assert(uo_out == 8'd225); // ement 15×15
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+        $display("¡Todas las pruebas pasaron!");
+        $finish;
+    end
 
+    always #5 clk = ~clk;
 endmodule
